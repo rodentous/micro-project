637b663eac1a1b8b3a3e4325b01b8abc9c0c4174
rodentous <therodentous@gmail.com>
2025-02-22 12:20:16 +0300

    init

---
+++ build/build_files/obj/x64/Debug/micro-project/main.d
@@ -0,0 +1,6 @@
+obj/x64/Debug/micro-project/main.o: ../../src/main.cpp \
+ /usr/include/stdc-predef.h ../external/raylib-master/src/raylib.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/include/stdarg.h
+/usr/include/stdc-predef.h:
+../external/raylib-master/src/raylib.h:
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/include/stdarg.h:
